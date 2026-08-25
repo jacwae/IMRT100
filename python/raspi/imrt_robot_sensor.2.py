1f7f83559c15d332f5a405e9390c53e646c684b0
jacwae <jacob.waersten@gmail.com>
2026-08-25 14:46:46 +0200

    t

--- python/raspi/imrt_robot_sensor.2.py
+++ python/raspi/imrt_robot_sensor.2.py
@@ -77,7 +77,7 @@ try:
             motor_speed_2 = -80
 
         # Hindring på høyre side: sving venstre.
-        elif dist_3 < obstacle_threshold_cm and dist_2 > 15:
+        elif dist_3 < obstacle_threshold_cm or dist_3 > 50:
             motor_speed_1 = -80
             motor_speed_2 = 120
 
